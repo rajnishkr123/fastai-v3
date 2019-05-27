4498ddf994370eb91b226d361d968d1b0b45df78
rajnishkr123 <rajnish_kr_123@yahoo.co.in>
2019-05-27 16:24:54 -0500

    new model

--- app/server.py
+++ app/server.py
@@ -14,7 +14,7 @@ export_file_name = 'export.pkl'
 
 # classes = ['black', 'grizzly', 'teddys']
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1YXCkzM_KtUXLyMQcqajGLSX13kIwqclh'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1uLPsGPHMq3HBYH6bEORwQKvXb96Qbiji'
 
 classes = ['audi', 'cadillac', 'ferrari', 'jaguar', 'lamborghini', 'maruti', 'mercedes', 'mustang', 'porsche', 'tata']
 path = Path(__file__).parent
