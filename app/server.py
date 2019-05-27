02a0b99e7acff03e0a106460122f90f7d0bb3489
rajnishkr123 <rajnish_kr_123@yahoo.co.in>
2019-05-26 20:32:43 -0500

    test again

--- app/server.py
+++ app/server.py
@@ -9,15 +9,14 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+# classes = ['black', 'grizzly', 'teddys']
 
-# export_file_url = 'https://drive.google.com/file/d/1YXCkzM_KtUXLyMQcqajGLSX13kIwqclh/view?usp=sharing'
-# export_file_name = 'Car_classifier_model.pkl'
+export_file_url = 'https://drive.google.com/file/d/1YXCkzM_KtUXLyMQcqajGLSX13kIwqclh/view?usp=sharing'
 
-# classes = ['audi', 'cadillac', 'ferrari', 'jaguar', 'lamborghini', 'maruti', 'mercedes', 'mustang', 'porsche', 'tata']
+classes = ['audi', 'cadillac', 'ferrari', 'jaguar', 'lamborghini', 'maruti', 'mercedes', 'mustang', 'porsche', 'tata']
 path = Path(__file__).parent
 
 app = Starlette()
